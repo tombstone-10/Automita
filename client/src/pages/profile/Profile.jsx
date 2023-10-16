42a6926492397026880cf153f8d0628d55e072b3
Ali Ahmed <tombstone.act@gmail.com>
2023-10-17 04:23:17 +0500

    Profile Default Page
    The default page for profile added, lottie installed and its gif used.

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,10 +1,64 @@
 import Tabs from "../../components/Tabs";
+import Lottie from "react-lottie";
+import "./Profile.css";
+import gif from "../../assets/gif/profile-gif.json";
+import { FaCheck } from "react-icons/fa";
 
 const Profile = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: gif,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <>
       <Tabs />
-      <h2>Hello I am Profile</h2>
+      <div className="profile-container">
+        <div className="profile-form">
+          <h2 className="profile-heading">Let's get started!</h2>
+          <div className="profile-intro-row">
+            <div className="profile-box">
+              <div className="profile-box-icon">
+                <FaCheck />
+              </div>
+              <p>Add courses, teachers, programs.</p>
+            </div>
+          </div>
+          <div className="profile-intro-row">
+            <div className="profile-box">
+              <div className="profile-box-icon">
+                <FaCheck />
+              </div>
+              <p>Assign the courses, teachers to programs.</p>
+            </div>
+          </div>
+          <div className="profile-intro-row">
+            <div className="profile-box">
+              <div className="profile-box-icon">
+                <FaCheck />
+              </div>
+              <p>Simply generate timetable.</p>
+            </div>
+          </div>
+          <div className="profile-intro-row">
+            <div className="profile-box">
+              <div className="profile-box-icon">
+                <FaCheck />
+              </div>
+              <p>
+                View the timetable with respect to programs, classrooms,
+                teachers.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-container">
+          <Lottie options={defaultOptions} height={300} width={600} />
+        </div>
+      </div>
     </>
   );
 };
