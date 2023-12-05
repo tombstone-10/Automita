07bd6d317a55a875dafb7e8a5785490324014c21
Umar Rasheed <umarrasheed.jutt.9@gmail.com>
2023-12-05 07:08:33 +0500

    Data is generate tabs are set to dynamic
    Removed the data in the generate tab which is static (directly written in HTML), and make it dynamic. Now, the data is coming from states.

--- client/src/components/GenerateTable.jsx
+++ client/src/components/GenerateTable.jsx
@@ -1,7 +1,58 @@
 import "./GenerateTable.css";
 import { FaTrash } from "react-icons/fa";
+import { useState } from 'react';
 
 const GenerateTable = ({ parentName }) => {
+  const [classes_addition, set_classes_addition] = useState([{ programName: 'BSSE', session: 'Fall', semester: '1', section: 'A' },
+  { programName: 'BSSE', session: 'Fall', semester: '1', section: 'A' },
+  { programName: 'BSSE', session: 'Fall', semester: '3', section: 'A' },
+  { programName: 'BSSE', session: 'Fall', semester: '5', section: 'A' },
+  { programName: 'BSSE', session: 'Fall', semester: '7', section: 'A' },
+  { programName: 'BSSE', session: 'Fall', semester: '7', section: 'B' },
+  { programName: 'BSSE', session: 'Fall', semester: '7', section: 'B' },
+  { programName: 'BSSE', session: 'Fall', semester: '7', section: 'B' },
+  { programName: 'BSSE', session: 'Fall', semester: '7', section: 'B' },
+  { programName: 'BSSE', session: 'Fall', semester: '7', section: 'B' },
+  { programName: 'BSSE', session: 'Fall', semester: '7', section: 'B' },
+  { programName: 'BSSE', session: 'Fall', semester: '7', section: 'B' },
+  { programName: 'BSSE', session: 'Fall', semester: '7', section: 'B' },
+  ]);
+  const [course_addition, set_course_addition] = useState([{ course_code: 'SE301', course_name: 'Software Re-Engineering', credit_hour: '3', assigned_to: 'BSSE-7-A, BSSE-7-B' },
+  { course_code: 'SE301', course_name: 'Software Re-Engineering', credit_hour: '3', assigned_to: 'BSSE-7-A, BSSE-7-B' },
+  { course_code: 'SE336', course_name: 'Mobile Application Development', credit_hour: '3', assigned_to: 'BSSE-7-A, BSSE-7-B' },
+  { course_code: 'SE464', course_name: 'Big Data Analytics', credit_hour: '3', assigned_to: 'BSSE-7-A, BSSE-7-B' },
+  { course_code: 'SE421', course_name: 'Software Project Management', credit_hour: '3', assigned_to: 'BSSE-7-A, BSSE-7-B' },
+  { course_code: 'SE301', course_name: 'Software Re-Engineering', credit_hour: '3', assigned_to: 'BSSE-7-A, BSSE-7-B' },
+  { course_code: 'SE301', course_name: 'Software Re-Engineering', credit_hour: '3', assigned_to: 'BSSE-7-A, BSSE-7-B' },
+  { course_code: 'SE301', course_name: 'Software Re-Engineering', credit_hour: '3', assigned_to: 'BSSE-7-A, BSSE-7-B' },
+  { course_code: 'SE301', course_name: 'Software Re-Engineering', credit_hour: '3', assigned_to: 'BSSE-7-A, BSSE-7-B' },
+  { course_code: 'SE301', course_name: 'Software Re-Engineering', credit_hour: '3', assigned_to: 'BSSE-7-A, BSSE-7-B' },
+  { course_code: 'SE301', course_name: 'Software Re-Engineering', credit_hour: '3', assigned_to: 'BSSE-7-A, BSSE-7-B' },
+  { course_code: 'SE301', course_name: 'Software Re-Engineering', credit_hour: '3', assigned_to: 'BSSE-7-A, BSSE-7-B' },
+  { course_code: 'SE301', course_name: 'Software Re-Engineering', credit_hour: '3', assigned_to: 'BSSE-7-A, BSSE-7-B' },
+
+  ]);
+  const [teacher_addition, set_teacher_addition] = useState ([{teacher_name: 'Dr. Iqbal Murtaza', assigned_classes:'BSSE-F-3-B, BSSE-F-3-A',
+  assigned_courses:'Data Structures & Algorithms, Formal Methods in Software'},
+  {teacher_name: 'Mr. Adnan Aslam', assigned_classes:'BSSE-F-3-B, BSSE-F-3-A',
+  assigned_courses:'Mobile Application Development, Big Data Analysis'},
+  {teacher_name: 'Dr. Iqbal Murtaza', assigned_classes:'BSSE-F-3-B, BSSE-F-3-A',
+  assigned_courses:'Data Structures & Algorithms, Formal Methods in Software'},
+  {teacher_name: 'Dr. Iqbal Murtaza', assigned_classes:'BSSE-F-3-B, BSSE-F-3-A',
+  assigned_courses:'Data Structures & Algorithms, Formal Methods in Software'},
+
+
+  ])
+  const [room_addition, set_room_addition] = useState ([
+    {room_no:'401'},
+    {room_no:'402'},
+    {room_no:'403'},
+    {room_no:'404'},
+    {room_no:'405'},
+    {room_no:'406'},
+    {room_no:'407'},
+    {room_no:'401 - Lab'}
+  ])
   return (
     <>
       {parentName == "addClass" && (
@@ -14,123 +65,18 @@ const GenerateTable = ({ parentName }) => {
               <th>Section</th>
               <th></th>
             </tr>
-            <tr>
-              <td>BSSE</td>
-              <td>Fall</td>
-              <td>1</td>
-              <td>A</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>BSSE</td>
-              <td>Fall</td>
-              <td>3</td>
-              <td>A</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>BSSE</td>
-              <td>Fall</td>
-              <td>5</td>
-              <td>A</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>BSSE</td>
-              <td>Fall</td>
-              <td>7</td>
-              <td>A</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>BSSE</td>
-              <td>Fall</td>
-              <td>7</td>
-              <td>B</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>BSSE</td>
-              <td>Fall</td>
-              <td>7</td>
-              <td>B</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>BSSE</td>
-              <td>Fall</td>
-              <td>7</td>
-              <td>B</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>BSSE</td>
-              <td>Fall</td>
-              <td>7</td>
-              <td>B</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>BSSE</td>
-              <td>Fall</td>
-              <td>7</td>
-              <td>B</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>BSSE</td>
-              <td>Fall</td>
-              <td>7</td>
-              <td>B</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>BSSE</td>
-              <td>Fall</td>
-              <td>7</td>
-              <td>B</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>BSSE</td>
-              <td>Fall</td>
-              <td>7</td>
-              <td>B</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>BSSE</td>
-              <td>Fall</td>
-              <td>7</td>
-              <td>B</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
+            {classes_addition.map((classes, index) => (
+              <tr key={index}>
+                <td>{classes.programName}</td>
+                <td>{classes.session}</td>
+                <td>{classes.semester}</td>
+                <td>{classes.section}</td>
+                <td className="del">
+                  <FaTrash />
+                </td>
+              </tr>
+            ))}
+
           </table>
         </div>
       )}
@@ -145,42 +91,17 @@ const GenerateTable = ({ parentName }) => {
               <th>Assigned To</th>
               <th></th>
             </tr>
-            <tr>
-              <td>SE301</td>
-              <td>Software Re-Engineering</td>
-              <td>3</td>
-              <td>BSSE-7-A, BSSE-7-B</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>SE336</td>
-              <td>Mobile Application Development</td>
-              <td>3</td>
-              <td>BSSE-7-A, BSSE-7-B</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>SE464</td>
-              <td>Big Data Analytics</td>
-              <td>3</td>
-              <td>BSSE-7-A, BSSE-7-B</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>SE421 </td>
-              <td>Software Project Management</td>
-              <td>3</td>
-              <td>BSSE-7-A, BSSE-7-B</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
+            {course_addition.map((courses, index) => (
+              <tr key={index}>
+                <td>{courses.course_code}</td>
+                <td>{courses.course_name}</td>
+                <td>{courses.credit_hour}</td>
+                <td>{courses.assigned_to}</td>
+                <td className="del">
+                  <FaTrash />
+                </td>
+              </tr>
+            ))}
           </table>
         </div>
       )}
@@ -194,25 +115,16 @@ const GenerateTable = ({ parentName }) => {
               <th>Assigned Course(es)</th>
               <th></th>
             </tr>
-            <tr>
-              <td>Dr. Iqbal Murtaza</td>
-              <td>BSSE-F-3-B, BSSE-F-3-A</td>
-              <td>
-                Data Structures & Algorithms, Formal Methods in Software
-                Engineering
-              </td>
+            {teacher_addition.map((teachers,index)=> (
+              <tr key={index}>
+              <td>{teachers.teacher_name}</td>
+              <td>{teachers.assigned_classes}</td>
+              <td>{teachers.assigned_courses}</td>
               <td className="del">
                 <FaTrash />
               </td>
             </tr>
-            <tr>
-              <td>Mr. Adnan Aslam</td>
-              <td>BSSE-F-7-A, BSSE-F-7-B</td>
-              <td>Mobile Application Development, Big Data Analytics</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
+            ))}
           </table>
         </div>
       )}
@@ -224,48 +136,14 @@ const GenerateTable = ({ parentName }) => {
               <th>Room Name</th>
               <th></th>
             </tr>
-            <tr>
-              <td>401</td>
-              <td className="del">
+            {room_addition.map((rooms,index) => (
+              <tr key={index}>
+                <td>{rooms.room_no}</td>
+                <td className="del">
                 <FaTrash />
               </td>
-            </tr>
-            <tr>
-              <td>402</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>403</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>404</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>405</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>406</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
-            <tr>
-              <td>407</td>
-              <td className="del">
-                <FaTrash />
-              </td>
-            </tr>
+              </tr>
+            ))}
           </table>
         </div>
       )}
