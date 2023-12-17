138c9df0d535c22f3998331bf0e73a38c6adde4f
Ali Ahmed <tombstone.act@gmail.com>
2023-12-17 16:16:05 +0500

    Data added

--- client/src/components/Timetable/TimetableClass.jsx
+++ client/src/components/Timetable/TimetableClass.jsx
@@ -1,43 +1,290 @@
 export const bsse1a = [
-  { day: 'Monday', time: '8:00 AM', class: 'Class A' },
-  { day: 'Tuesday', time: '10:00 AM', class: 'Class B' },
-  { day: 'Wednesday', time: '1:00 PM', class: 'Class C' },
+  {
+    day: "Monday",
+    time: "8:00 AM",
+    class: "Foundational Mathematics (CT-401)",
+  },
+  {
+    day: "Monday",
+    time: "9:00 AM",
+    class: "Foundational Mathematics (CT-401)",
+  },
+  {
+    day: "Monday",
+    time: "10:00 AM",
+    class: "Application ICT (CT-401)",
+  },
+  {
+    day: "Monday",
+    time: "10:00 AM",
+    class: "Application ICT (CT-401)",
+  },
+  {
+    day: "Monday",
+    time: "12:00 PM",
+    class: "Functional English (CT-401)",
+  },
+  {
+    day: "Monday",
+    time: "1:00 PM",
+    class: "Functional English (CT-401)",
+  },
+  {
+    day: "Monday",
+    time: "2:00 PM",
+    class: "Programming Fundamentals (CT-401)",
+  },
+  {
+    day: "Tuesday",
+    time: "8:00 AM",
+    class: "Programming Fundamentals Lab (Lab 401)",
+  },
+  {
+    day: "Tuesday",
+    time: "9:00 AM",
+    class: "Programming Fundamentals Lab (Lab 401)",
+  },
+  {
+    day: "Tuesday",
+    time: "10:00 AM",
+    class: "Programming Fundamentals Lab (Lab 401)",
+  },
+  {
+    day: "Tuesday",
+    time: "11:00 AM",
+    class: "Applied Physics (CT-401)",
+  },
+  {
+    day: "Tuesday",
+    time: "12:00 PM",
+    class: "Applied Physics (CT-401)",
+  },
+  { day: "Wednesday", time: "8:00 AM", class: "Application ICT Lab (Lab 401)" },
+  { day: "Wednesday", time: "9:00 AM", class: "Application ICT Lab (Lab 401)" },
+  {
+    day: "Wednesday",
+    time: "10:00 AM",
+    class: "Application ICT Lab (Lab 401)",
+  },
+  { day: "Wednesday", time: "11:00 AM", class: "Application ICT (CT-401)" },
+  {
+    day: "Wednesday",
+    time: "12:00 PM",
+    class: "Islamic Studies & Ethics (CT-401)",
+  },
+  {
+    day: "Wednesday",
+    time: "1:00 PM",
+    class: "Islamic Studies & Ethics (CT-401)",
+  },
+  {
+    day: "Wednesday",
+    time: "2:00 PM",
+    class: "Functional English (CT-401)",
+  },
+  {
+    day: "Thursday",
+    time: "8:00 AM",
+    class: "Applied Physics Lab (CT-Physics Lab)",
+  },
+  {
+    day: "Thursday",
+    time: "9:00 AM",
+    class: "Applied Physics Lab (CT-Physics Lab)",
+  },
+  {
+    day: "Thursday",
+    time: "10:00 AM",
+    class: "Applied Physics Lab (CT-Physics Lab)",
+  },
+  {
+    day: "Thursday",
+    time: "11:00 AM",
+    class: "Programming Fundamentals (CT-401)",
+  },
+  {
+    day: "Thursday",
+    time: "12:00 PM",
+    class: "Programming Fundamentals (CT-401)",
+  },
+  {
+    day: "Thursday",
+    time: "1:00 PM",
+    class: "Foundational Mathematics (CT-401)",
+  },
 ];
 
 // Timetable for BSSE-1-B
 export const bsse1b = [
-  { day: 'Monday', time: '12:00 PM', class: 'Class A' },
-  { day: 'Wednesday', time: '4:00 PM', class: 'Class B' },
-  { day: 'Friday', time: '1:00 PM', class: 'Class C' },
+  {
+    day: "Monday",
+    time: "8:00 AM",
+    class: "Applied Physics Lab (CT-Physics Lab)",
+  },
+  {
+    day: "Monday",
+    time: "9:00 AM",
+    class: "Applied Physics Lab (CT-Physics Lab)",
+  },
+  {
+    day: "Monday",
+    time: "10:00 AM",
+    class: "Applied Physics Lab (CT-Physics Lab)",
+  },
+  {
+    day: "Monday",
+    time: "11:00 AM",
+    class: "Application ICT (CT-401)",
+  },
+  {
+    day: "Monday",
+    time: "12:00 PM",
+    class: "Application ICT (CT-402)",
+  },
+  {
+    day: "Monday",
+    time: "1:00 PM",
+    class: "Foundational Mathematics (CT-402)",
+  },
+  {
+    day: "Monday",
+    time: "1:00 PM",
+    class: "Programming Fundamentals (CT-402)",
+  },
+  {
+    day: "Tuesday",
+    time: "8:00 AM",
+    class: "Applied Physics (CT-402)",
+  },
+  {
+    day: "Tuesday",
+    time: "9:00 AM",
+    class: "Applied Physics (CT-402)",
+  },
+  {
+    day: "Tuesday",
+    time: "10:00 AM",
+    class: "Foundational Mathematics (CT-402)",
+  },
+  {
+    day: "Tuesday",
+    time: "2:00 PM",
+    class: "Functional English (CT-402)",
+  },
+  {
+    day: "Tuesday",
+    time: "11:00 AM",
+    class: "Application ICT Lab (Lab 401)",
+  },
+  {
+    day: "Tuesday",
+    time: "12:00 PM",
+    class: "Application ICT Lab (Lab 401)",
+  },
+  {
+    day: "Tuesday",
+    time: "1:00 PM",
+    class: "Application ICT Lab (Lab 401)",
+  },
+  {
+    day: "Wednesday",
+    time: "8:00 AM",
+    class: "Programming Fundamentals (CT-402)",
+  },
+  {
+    day: "Wednesday",
+    time: "9:00 AM",
+    class: "Programming Fundamentals (CT-402)",
+  },
+  {
+    day: "Wednesday",
+    time: "10:00 AM",
+    class: "Programming Fundamentals (CT-402)",
+  },
+  {
+    day: "Wednesday",
+    time: "11:00 AM",
+    class: "Programming Fundamentals Lab (Lab 401)",
+  },
+  {
+    day: "Wednesday",
+    time: "12:00 PM",
+    class: "Programming Fundamentals Lab (Lab 401)",
+  },
+  {
+    day: "Wednesday",
+    time: "1:00 PM",
+    class: "Programming Fundamentals Lab (Lab 401)",
+  },
+  {
+    day: "Wednesday",
+    time: "2:00 PM",
+    class: "Islamic Studies & Ethics (CT-402)",
+  },
+  {
+    day: "Thursday",
+    time: "8:00 AM",
+    class: "Application ICT Lab (Lab 401)",
+  },
+  {
+    day: "Thursday",
+    time: "9:00 AM",
+    class: "Application ICT Lab (Lab 401)",
+  },
+  {
+    day: "Thursday",
+    time: "10:00 AM",
+    class: "Application ICT Lab (Lab 401)",
+  },
+  {
+    day: "Thursday",
+    time: "11:00 AM",
+    class: "Islamic Studies & Ethics (CT-402)",
+  },
+  {
+    day: "Thursday",
+    time: "12:00 PM",
+    class: "Foundational Mathematics (CT-402)",
+  },
+  {
+    day: "Thursday",
+    time: "1:00 PM",
+    class: "Functional English (CT-402)",
+  },
+  {
+    day: "Thursday",
+    time: "2:00 PM",
+    class: "Functional English (CT-402)",
+  },
 ];
 
 export const bsse3a = [
-  { day: 'Monday', time: '12:00 PM', class: 'Class A' },
-  { day: 'Wednesday', time: '4:00 PM', class: 'Class B' },
-  { day: 'Friday', time: '1:00 PM', class: 'Class C' },
+  { day: "Monday", time: "12:00 PM", class: "Class A" },
+  { day: "Wednesday", time: "4:00 PM", class: "Class B" },
+  { day: "Friday", time: "1:00 PM", class: "Class C" },
 ];
 export const bsse3b = [
-  { day: 'Monday', time: '12:00 PM', class: 'Class A' },
-  { day: 'Wednesday', time: '4:00 PM', class: 'Class B' },
-  { day: 'Friday', time: '1:00 PM', class: 'Class C' },
+  { day: "Monday", time: "12:00 PM", class: "Class A" },
+  { day: "Wednesday", time: "4:00 PM", class: "Class B" },
+  { day: "Friday", time: "1:00 PM", class: "Class C" },
 ];
 export const bsse5a = [
-  { day: 'Monday', time: '12:00 PM', class: 'Class A' },
-  { day: 'Wednesday', time: '4:00 PM', class: 'Class B' },
-  { day: 'Friday', time: '1:00 PM', class: 'Class C' },
+  { day: "Monday", time: "12:00 PM", class: "Class A" },
+  { day: "Wednesday", time: "4:00 PM", class: "Class B" },
+  { day: "Friday", time: "1:00 PM", class: "Class C" },
 ];
 export const bsse5b = [
-  { day: 'Monday', time: '12:00 PM', class: 'Class A' },
-  { day: 'Wednesday', time: '4:00 PM', class: 'Class B' },
-  { day: 'Friday', time: '1:00 PM', class: 'Class C' },
+  { day: "Monday", time: "12:00 PM", class: "Class A" },
+  { day: "Wednesday", time: "4:00 PM", class: "Class B" },
+  { day: "Friday", time: "1:00 PM", class: "Class C" },
 ];
 export const bsse7a = [
-  { day: 'Monday', time: '12:00 PM', class: 'Class A' },
-  { day: 'Wednesday', time: '4:00 PM', class: 'Class B' },
-  { day: 'Tuesday', time: '1:00 PM', class: 'Class C' },
+  { day: "Monday", time: "12:00 PM", class: "Class A" },
+  { day: "Wednesday", time: "4:00 PM", class: "Class B" },
+  { day: "Tuesday", time: "1:00 PM", class: "Class C" },
 ];
 export const bsse7b = [
-  { day: 'Tuesday', time: '12:00 PM', class: 'Class A' },
-  { day: 'Wednesday', time: '4:00 PM', class: 'Class B' },
-  { day: 'Friday', time: '1:00 PM', class: 'Class C' },
+  { day: "Tuesday", time: "12:00 PM", class: "Class A" },
+  { day: "Wednesday", time: "4:00 PM", class: "Class B" },
+  { day: "Friday", time: "1:00 PM", class: "Class C" },
 ];
--- client/src/pages/view/ViewTeacher.jsx
+++ client/src/pages/view/ViewTeacher.jsx
@@ -5,24 +5,38 @@ import { useLocation } from "react-router-dom";
 import VerticalTabs from "../../components/Tabs/VerticalTabs";
 import Lottie from "react-lottie";
 import gif from "../../assets/gif/profile-gif.json";
-import { bsse1a, bsse1b, bsse3a, bsse3b, bsse5a, bsse5b, bsse7a, bsse7b, } from "../../components/Timetable/TimetableClass";
+import {
+  bsse1a,
+  bsse1b,
+  bsse3a,
+  bsse3b,
+  bsse5a,
+  bsse5b,
+  bsse7a,
+  bsse7b,
+} from "../../components/Timetable/TimetableClass";
+import { driqbalmurtaza } from "../../components/Timetable/TimetableTeacher";
 const ViewTeacher = () => {
   const location = useLocation();
-  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
   const timeslots = [
-    '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM',
-    '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM'
+    "8:00 AM",
+    "9:00 AM",
+    "10:00 AM",
+    "11:00 AM",
+    "12:00 PM",
+    "1:00 PM",
+    "2:00 PM",
+    "3:00 PM",
+    "4:00 PM",
   ];
   var timetableData = [];
   if (location.pathname !== "/view/teacher") {
-    if (location.pathname === '/view/teacher/dr-iqbal-murtaza') {
-      if (bsse1a)
-        timetableData = bsse1a;
-      else
-        timetableData = [];
+    if (location.pathname === "/view/teacher/dr-iqbal-murtaza") {
+      if (driqbalmurtaza) timetableData = driqbalmurtaza;
+      else timetableData = [];
     }
-  }
-  else {
+  } else {
     timetableData = [];
   }
 
@@ -48,10 +62,12 @@ const ViewTeacher = () => {
                 <tr key={timeIndex}>
                   <td className="time-cell">{time}</td>
                   {days.map((day, dayIndex) => {
-                    const classData = timetableData.find(item => item.day === day && item.time === time);
+                    const classData = timetableData.find(
+                      (item) => item.day === day && item.time === time
+                    );
                     return (
                       <td key={dayIndex} className="class-cell">
-                        {classData ? classData.class : ''}
+                        {classData ? classData.class : ""}
                       </td>
                     );
                   })}
