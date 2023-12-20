5571a6b5dbb7dbecb4eeb2852a494a8127d7949e
Ali Ahmed <tombstone.act@gmail.com>
2023-12-20 12:40:38 +0500

    Generate Button Update

--- client/src/pages/generate/AddClass.jsx
+++ client/src/pages/generate/AddClass.jsx
@@ -1,18 +1,39 @@
+import { useState } from "react";
 import Add from "../../components/Add/Add";
 import Tabs from "../../components/Tabs/Tabs";
 import { generateTabs } from "../../data/TabsData";
 import "./Generate.css";
+import Loading from "../../components/Loading";
 
 const AddClass = () => {
-
+  const [modal, setModal] = useState(false);
+  const generatePop = () => {
+    setModal(!modal);
+  }
   return (
     <>
       <div className="scroll-container">
         <Tabs tabs={generateTabs} />
         <div className="generateBtnContainer">
-          <button>Generate</button>
+          <a href="#generate">
+          <button  onClick={generatePop}>Generate</button>
+          </a>
         </div>
+        {modal && (<div className="popup-container">
+          <div className="generate-form">
+          <div className="gen">
+            <Loading/>
+            <h3 className="gen-h">Generating Timetable...</h3>
+            <h4 className="gen-h">Please Wait</h4>
+            <a href="#cancel">
+              <button className="cancel-btn" onClick={generatePop}>Cancel</button>
+            </a>
+          </div>
+          </div>
+          
+        </div>)}
         <Add parentName={"addClass"} />
+        
       </div>
     </>
   );
